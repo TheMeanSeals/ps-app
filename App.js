5d1b949d03a6dbaf5ce339726375318f32327ebf
Pontus <pontusss85@gmail.com>
2018-04-22 15:43:58 +0200

    Ejectade

--- App.js
+++ App.js
@@ -5,9 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>Det här är snart världens bästa terapiapp!</Text>
       </View>
     );
   }
